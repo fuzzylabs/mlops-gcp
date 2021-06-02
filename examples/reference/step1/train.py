9f263773cc84609441bf2b983feaf2d5804a4f85
D. Lowl <d.lowl@d-lowl.space>
2021-06-02 11:16:07 +0500

    Add DVC pipeline, model saving and metric export

--- examples/reference/step1/train.py
+++ examples/reference/step1/train.py
@@ -1,12 +1,23 @@
-from mnist import MNIST
+import json
+
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+import pickle
+import joblib
+import os
+import argparse
+
+parser = argparse.ArgumentParser("Train model")
+parser.add_argument('--model-dir', dest='model_dir', default=os.getenv("AIP_MODEL_DIR"))
+
+args = parser.parse_args()
 
 # Load whole dataset
-mndata = MNIST('../data/fashion-mnist', gz=True)
+with open("../data/fashion-mnist/train.pickle", "rb") as f:
+    train_images, train_labels = pickle.load(f)
 
-train_images, train_labels = mndata.load_training()
-test_images, test_labels = mndata.load_testing()
+with open("../data/fashion-mnist/test.pickle", "rb") as f:
+    test_images, test_labels = pickle.load(f)
 
 # Define the simplest SVC model
 model = GaussianNB()
@@ -17,4 +28,25 @@ model.fit(train_images, train_labels)
 
 # Test model
 predicted_labels = model.predict(test_images)
-print(accuracy_score(list(test_labels), predicted_labels))
+accuracy = accuracy_score(list(test_labels), predicted_labels)
+print("Accuracy:", accuracy)
+with open("metrics.json", "w") as f:
+    json.dump({
+        "accuracy": accuracy
+    }, f, indent=2)
+
+
+# Dump model
+if args.model_dir.startswith("gs://"):
+    with open('model.joblib', 'wb') as f:
+        joblib.dump(model, f)
+    from google.cloud import storage
+    from google.cloud.storage.blob import Blob
+    client = storage.Client()
+    print(client)
+    blob = Blob.from_string(os.path.join(args.model_dir, 'model.joblib'), client)
+    print(blob)
+    blob.upload_from_filename("model.joblib")
+else:
+    with open(os.path.join(args.model_dir, 'model.joblib'), 'wb') as f:
+        joblib.dump(model, f)
