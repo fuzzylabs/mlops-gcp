ac5a341f1855687c923b9bb2c7d90a469bb11e3f
D. Lowl <d.lowl@d-lowl.space>
2021-06-10 18:38:01 +0500

    Add Sacred experiment tracking to step1

--- examples/reference/README.md
+++ examples/reference/README.md
@@ -1,7 +1,31 @@
+## Preparation
+```
+python -m venv env/
+source env/bin/activate
+pip install -r requirements.txt
+dvc pull
+```
+
 ## Step 0 -- locally train a model
 ## Step 1 -- locally train with DVC pipeline
 * Copy `dvc-step1.yaml` to `dvc.yaml`
 * `dvc repro` to run training
+
+### Experiment tracking
+
+This step uses [Sacred](https://github.com/IDSIA/sacred) for experiment tracking. The results are saved to MongoDB.
+To run MongoDB locally with Docker:
+
+```
+docker run --name mongo -p 27017:27017 -d mongo:latest
+```
+
+To view the experiments, you can use [Omniboard](https://vivekratnavel.github.io/omniboard/#/README). To start Omniboard locally with the local MongoDB:
+```
+docker run -it --rm -p 9000:9000 --network host --name omniboard vivekratnavel/omniboard -m localhost:27017:sacred
+```
+
+and open dashboard in the browser [http://localhost:9000](http://localhost:9000)
 ## Step 2 -- train on Vertex AI
 * Copy `dvc-step2.yaml` to `dvc.yaml`
 * `dvc repro` to run training
--- examples/reference/step1/train.py
+++ examples/reference/step1/train.py
@@ -1,55 +1,84 @@
 import json
 
+from dvc.repo import Repo
+from dvc.path_info import PathInfo
+from funcy import reraise
+from dvc.exceptions import OutputNotFoundError, PathMissingError
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 import pickle
 import joblib
 import os
-import argparse
 import yaml
+from sacred import Experiment
+from sacred.observers import MongoObserver
 
-params = yaml.safe_load(open("../params.yaml"))["train"]
+ex = Experiment("fashion-mnist-model-training")
+ex.observers.append(MongoObserver(
+    url="mongodb://localhost:27017"
+))
 
-parser = argparse.ArgumentParser("Train model")
-parser.add_argument('--model-dir', dest='model_dir', default=os.getenv("AIP_MODEL_DIR"))
+def dvc_get_md5(path, repo=None, rev=None, remote=None):
+    with Repo.open(repo, rev=rev, subrepos=True, uninitialized=True) as _repo:
+        path_info = PathInfo(_repo.root_dir) / path
+        with reraise(FileNotFoundError, PathMissingError(path, repo)):
+            metadata = _repo.repo_fs.metadata(path_info)
 
-args = parser.parse_args()
+        if not metadata.is_dvc:
+            raise OutputNotFoundError(path, repo)
 
-# Load whole dataset
-with open("../data/fashion-mnist/train.pickle", "rb") as f:
-    train_images, train_labels = pickle.load(f)
+        md5 = metadata.repo.dvcfs.info(path_info)["md5"]
+        return md5
 
-with open("../data/fashion-mnist/test.pickle", "rb") as f:
-    test_images, test_labels = pickle.load(f)
+@ex.config
+def config():
+    n_neighbors = yaml.safe_load(open("../params.yaml"))["train"]["n_neighbours"]
+    model_dir = os.getenv("AIP_MODEL_DIR", "../model/")
+    train_md5 = dvc_get_md5("data/fashion-mnist/train.pickle")
+    test_md5 = dvc_get_md5("data/fashion-mnist/test.pickle")
 
-# Define the simplest SVC model
-model = KNeighborsClassifier(n_neighbors=params["n_neighbours"])
-print(model)
+@ex.automain
+def main(n_neighbors, model_dir, _run):
+    # Load whole dataset
+    with open("../data/fashion-mnist/train.pickle", "rb") as f:
+        train_images, train_labels = pickle.load(f)
 
-# Train model
-model.fit(train_images, train_labels)
+    with open("../data/fashion-mnist/test.pickle", "rb") as f:
+        test_images, test_labels = pickle.load(f)
 
-# Test model
-predicted_labels = model.predict(test_images)
-accuracy = accuracy_score(list(test_labels), predicted_labels)
-print("Accuracy:", accuracy)
-with open("../metrics.json", "w") as f:
-    json.dump({
-        "accuracy": accuracy
-    }, f, indent=2)
+    # Define the simplest SVC model
+    model = KNeighborsClassifier(n_neighbors=n_neighbors)
+    print(model)
 
+    # Train model
+    model.fit(train_images, train_labels)
 
-# Dump model
-if args.model_dir.startswith("gs://"):
-    with open('model.joblib', 'wb') as f:
-        joblib.dump(model, f)
-    from google.cloud import storage
-    from google.cloud.storage.blob import Blob
-    client = storage.Client()
-    print(client)
-    blob = Blob.from_string(os.path.join(args.model_dir, 'model.joblib'), client)
-    print(blob)
-    blob.upload_from_filename("model.joblib")
-else:
-    with open(os.path.join(args.model_dir, 'model.joblib'), 'wb') as f:
-        joblib.dump(model, f)
+    # Test model
+    predicted_labels = model.predict(test_images)
+    accuracy = accuracy_score(list(test_labels), predicted_labels)
+    _run.log_scalar("accuracy", accuracy)
+    print("Accuracy:", accuracy)
+    with open("../metrics.json", "w") as f:
+        json.dump({
+            "accuracy": accuracy
+        }, f, indent=2)
+
+    # Dump model
+    if model_dir.startswith("gs://"):
+        with open('model.joblib', 'wb') as f:
+            joblib.dump(model, f)
+        from google.cloud import storage
+        from google.cloud.storage.blob import Blob
+        client = storage.Client()
+        print(client)
+        blob = Blob.from_string(os.path.join(model_dir, 'model.joblib'), client)
+        print(blob)
+        blob.upload_from_filename("model.joblib")
+    else:
+        with open(os.path.join(model_dir, 'model.joblib'), 'wb') as f:
+            joblib.dump(model, f)
+
+    return dict(
+        model_md5=dvc_get_md5("model/model.joblib")
+    )
